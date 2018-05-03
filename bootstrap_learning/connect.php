4becf52ab7f8855f519c8c0bd2b75caceb4acd73
Karthik Jeeva <jeevakarthik1993@rediffmail.com>
2018-05-03 13:27:34 +0200

    'rmoved'

--- bootstrap_learning/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("HOSTNAME","localhost");
-define("USERNAME","root");
-define("PASSWORD","");
-define("DATABASE","resume");
-$dbhandle=new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE) or die("cannot connect to database");
-?>
